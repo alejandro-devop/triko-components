6d72f9ebe8a306adfe3fa6273505f898561dd42c
Alejandro <alejandro.devop@gmail.com>
2020-08-27 16:57:33 -0500

    Added rejections button

---
+++ base/buttons/rejections-button/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View} from 'react-native';
+import IconButton from 'shared/components/base/buttons/icon-button';
+import useStyles from 'shared/hooks/use-styles';
+import Icon from 'shared/components/base/icon';
+import Text from 'shared/components/base/text';
+import styles from './styles';
+
+const RejectionsButton = ({readOnly, count}) => {
+  const [classes] = useStyles(styles);
+  return (
+    <View style={classes.root}>
+      {readOnly ? (
+        <Icon name="thumbs-down" style={classes.icon} />
+      ) : (
+        <IconButton name="thumbs-down" iconStyles={classes.icon} />
+      )}
+      {count > 0 && (
+        <View style={classes.countWrapper}>
+          <Text style={classes.countText} variant="caption">
+            {count}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default RejectionsButton;
