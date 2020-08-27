7d94002442a785fa293f667f335671f1c753f83d
Alejandro <alejandro.devop@gmail.com>
2020-08-27 18:25:46 -0500

    Added simple dialog

---
+++ dialogs/dialog/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {ScrollView, View} from 'react-native';
+import DialogBase from 'shared/components/dialogs/dialog-base';
+import Text from 'shared/components/base/text';
+import useStyles from 'shared/hooks/use-styles';
+import styles from './styles';
+
+/**
+ * This component allows to create a simple dialog with title
+ * @author Jorge Alejandro Quiroz Serna <jakop.box@gmail.com>
+ * @param contentStyles
+ * @param children
+ * @param classes
+ * @param open
+ * @param onClose
+ * @param title
+ * @returns {*}
+ * @constructor
+ */
+const Dialog = ({
+  contentStyles,
+  children,
+  disableScroll,
+  disableClose,
+  disableContent,
+  open,
+  onClose,
+  title,
+  loading,
+}) => {
+  const [classes] = useStyles(styles);
+  const WrapperComponent = disableScroll ? View : ScrollView;
+  return (
+    <DialogBase
+      loading={loading}
+      disableContent={disableContent}
+      contentStyles={contentStyles}
+      disableClose
+      disableCloseButton={disableClose}
+      open={open}
+      onClose={onClose}>
+      {title && (
+        <View style={classes.titleWrapper}>
+          <Text variant="subtitle" style={classes.title}>
+            {title}
+          </Text>
+        </View>
+      )}
+      <WrapperComponent
+        style={classes.scrollView}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={classes.scrollViewContent}>
+        {children}
+      </WrapperComponent>
+    </DialogBase>
+  );
+};
+
+Dialog.propTypes = {
+  animationTypes: PropTypes.oneOf(['none', 'slide', 'fade']),
+  contentStyles: PropTypes.any,
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  title: PropTypes.string,
+};
+
+export default Dialog;
---
+++ dialogs/dialog/styles.js
@@ -0,0 +1,16 @@
+export default ({palette}) => ({
+  scrollView: {
+    flex: 1,
+  },
+  scrollViewContent: {
+    flex: 1,
+  },
+  title: {
+    color: palette.blue,
+    textAlign: 'center',
+  },
+  titleWrapper: {
+    paddingVertical: 10,
+    marginBottom: 10,
+  },
+});
