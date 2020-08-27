8ea312c7449590d5ad3e818dfa83604e6ddb67f8
Alejandro <alejandro.devop@gmail.com>
2020-08-27 18:25:07 -0500

    Added Image Icon

---
+++ base/image-icon/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Image} from 'react-native';
+import useStyles from 'shared/hooks/use-styles';
+import styles from './styles';
+
+const Index = ({source, wrapperClass, imageClass}) => {
+  const [classes] = useStyles(styles);
+  return (
+    <View style={[classes.root, wrapperClass]}>
+      {source && <Image style={[classes.image, imageClass]} source={source} />}
+    </View>
+  );
+};
+
+export default Index;
---
+++ base/image-icon/styles.js
@@ -0,0 +1,11 @@
+export default {
+  root: {
+    width: 60,
+    height: 60,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+};
