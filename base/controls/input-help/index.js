aced1092cafc2ca61f2e1d97fa7ccd074343b09f
Alejandro <alejandro.devop@gmail.com>
2020-08-27 17:48:54 -0500

    Added input help

---
+++ base/controls/input-help/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View} from 'react-native';
+import Dialog from 'shared/components/dialogs/dialog';
+import Text from 'shared/components/base/text';
+import useStyles from 'shared/hooks/use-styles';
+import styles from './styles';
+import useTranslation from 'hooks/useTranslation';
+import Button from 'shared/components/base/buttons/button';
+
+const InputHelp = ({open, onClose, title, content}) => {
+  const [classes] = useStyles(styles);
+  const {_t} = useTranslation();
+  return (
+    <Dialog
+      title={_t(title)}
+      contentStyles={classes.dialog}
+      open={open}
+      onClose={onClose}>
+      <View style={classes.contentWrapper}>
+        <Text style={classes.text}>{_t(content)}</Text>
+      </View>
+      <View style={classes.actions}>
+        <Button primary onPress={onClose}>
+          {_t('accept_text')}
+        </Button>
+      </View>
+    </Dialog>
+  );
+};
+
+export default InputHelp;
---
+++ base/controls/input-help/styles.js
@@ -0,0 +1,17 @@
+export default () => ({
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  dialog: {
+    maxHeight: '90%',
+    height: '70%'
+  },
+  contentWrapper: {
+    paddingHorizontal: 20,
+    flex: 1,
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
