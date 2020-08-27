e6449b8faa4d8f12fd18bdbf80639c5d6cd0b869
Alejandro <alejandro.devop@gmail.com>
2020-08-27 16:52:50 -0500

    Added circle button

---
+++ base/buttons/circle-button/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {TouchableOpacity, View} from 'react-native';
+import Icon from 'shared/components/base/icon';
+import useStyles from 'shared/hooks/use-styles';
+import classNames from 'shared/utils/classnames';
+import CircleWave from 'shared/components/anims/CircleWave';
+import Text from 'shared/components/base/text';
+import componentStyles from './styles';
+
+/**
+ * this component allows to create a circle button with icon
+ * @author Jako <jakop.box@gmail.com>
+ * @version 1.0.1
+ * @app Client, Triko
+ * @param delay
+ * @param enableWave
+ * @param disabled
+ * @param name
+ * @param onPress
+ * @param primary
+ * @param size
+ * @param styles
+ * @returns {null|*}
+ * @constructor
+ */
+const CircleButton = ({
+  delay,
+  enableWave,
+  disabled,
+  label,
+  badge,
+  name,
+  onPress,
+  primary,
+  size = 'md',
+  styles = {},
+}) => {
+  const [classes] = useStyles(componentStyles);
+  if (!name) {
+    return null;
+  }
+  const WrapperComponent = disabled ? View : TouchableOpacity;
+  const content = (
+    <View style={classes.wrapper}>
+      <WrapperComponent
+        delay={delay}
+        style={classNames(
+          {
+            root: true,
+            primary,
+            disabled,
+            xl: size === 'xl',
+            lg: size === 'lg',
+            sm: size === 'sm',
+          },
+          [classes],
+          styles.root,
+        )}
+        onPress={() => (!disabled && onPress ? onPress() : null)}>
+        {Boolean(badge) && (
+          <View style={classes.badgeWrapper}>
+            <Text style={classes.badgeText}>{badge}</Text>
+          </View>
+        )}
+        <Icon
+          name={name}
+          style={classNames(
+            {
+              icon: true,
+              iconPrimary: primary,
+              iconXl: size === 'xl',
+              iconLg: size === 'lg',
+              iconSm: size === 'sm',
+            },
+            [classes],
+            styles.icon,
+          )}
+        />
+      </WrapperComponent>
+      {label && <Text style={classes.labelText}>{label}</Text>}
+    </View>
+  );
+  if (enableWave) {
+    return (
+      <CircleWave delay={0} duration={800}>
+        {content}
+      </CircleWave>
+    );
+  }
+  return content;
+};
+
+CircleButton.propTypes = {
+  onPress: PropTypes.func,
+};
+
+export default CircleButton;
---
+++ base/buttons/circle-button/styles.js
@@ -0,0 +1,59 @@
+export default ({palette, shadows}) => ({
+  root: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: '#FFF',
+    ...shadows.shadow1,
+    marginHorizontal: 5,
+  },
+  primary: {
+    backgroundColor: palette.blue,
+  },
+  icon: {
+    color: palette.blue,
+  },
+  iconPrimary: {
+    color: '#FFF',
+  },
+  sm: {
+    width: 40,
+    height: 40,
+  },
+  lg: {
+    width: 60,
+    height: 60,
+  },
+  xl: {
+    width: 75,
+    height: 75,
+  },
+  iconXl: {
+    fontSize: 30,
+  },
+  wrapper: {
+    alignItems: 'center',
+  },
+  labelText: {
+    color: palette.orange,
+    fontSize: 16,
+  },
+  badgeWrapper: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: palette.red,
+    paddingHorizontal: 10,
+    zIndex: 100,
+    borderRadius: 30,
+    transform: [{translateX: 10}, {translateY: -5}],
+  },
+  badgeText: {
+    color: '#FFF',
+  },
+  disabled: {
+    opacity: 0.3,
+  },
+});
