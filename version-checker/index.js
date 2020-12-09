e37d71ef370f5c749595ff5cd63c2dd98d19a727
Alejandro <alejandro.devop@gmail.com>
2020-12-09 09:32:35 -0500

    Fixes on version checker

--- version-checker/index.js
+++ version-checker/index.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import useRegionConfig from 'hooks/useRegionConfig';
 import NeedUpdate from './NeedUpdate';
-import {APP_VERSION} from 'react-native-dotenv';
+import {APP_VERSION, ENV} from 'react-native-dotenv';
+import {useQuery} from '@apollo/react-hooks';
+import {GET_REGION_CONFIG} from '../../../contexts/configuration/queries';
+import {useSession} from 'hooks/index';
 
 const VersionChecker = ({children}) => {
   const {appVersion} = useRegionConfig();
-  return appVersion === APP_VERSION ? children : <NeedUpdate />;
+  const {
+    stack: {regionId},
+    setKey,
+  } = useSession();
+  let timer = null;
+  const {refetch} = useQuery(GET_REGION_CONFIG, {
+    fetchPolicy: 'no-cache',
+    variables: {
+      region: regionId,
+    },
+    onCompleted: ({response}) => {
+      setKey('regionalConfig', response);
+    },
+  });
+
+  console.log('App version: ', appVersion, APP_VERSION, ENV);
+
+  const needUpdate = appVersion !== APP_VERSION;
+
+  useEffect(() => {
+    if (needUpdate) {
+      timer = setTimeout(() => {
+        refetch();
+      }, 10000);
+    } else {
+      clearTimeout(timer);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [needUpdate]);
+
+  return !needUpdate ? children : <NeedUpdate />;
 };
 
 export default VersionChecker;
