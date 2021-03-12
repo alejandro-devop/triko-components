1dd83d4e18614d2afbed5f29d438770fb63bd61e
Alejandro <alejandro.devop@gmail.com>
2021-03-11 21:04:30 -0500

    fixes on client

---
+++ complaints-wizard/complaints-form/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+import {useStyles} from 'hooks/index';
+import styles from './styles';
+
+const ComplaintsForm = () => {
+  const classes = useStyles(styles);
+  return (
+    <View style={classes.root}>
+      
+    </View>
+  );
+};
+
+export default ComplaintsForm;
---
+++ complaints-wizard/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const ComplaintsWizard = () => {
+  return null;
+};
+
+export default ComplaintsWizard;
